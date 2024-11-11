35cda30042ab7cdd215a2f8f4c183f8774c40671
Jack <jack.70aw@gmail.com>
2024-11-11 12:17:21 +0330

    add font

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const vazir = localFont({
+  src: "./fonts/Vazirmatn.ttf",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Next Home",
@@ -12,10 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
+    <html lang="en" className={vazir.className}>
+      <body>{children}</body>
     </html>
   );
 }
