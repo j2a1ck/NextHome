2b8759e29a83148e33163bad87f8b946c7e85797
Jack <jack.70aw@gmail.com>
2024-12-17 16:38:59 +0330

    fix: adjust footer layout and spacing for improved responsiveness

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -6,8 +6,8 @@ import { FaYoutube } from "react-icons/fa6";
 import Link from "next/link";
 const Footer = () => {
   return (
-    <footer className="mx-[180px] mt-10 flex h-[245px] justify-center text-right">
-      <div className="">
+    <footer className="mx-[10px] mt-10 flex h-[245px] justify-center text-right">
+      <div>
         <div className="flex flex-col lg:flex-row">
           <ul className="order-last my-10 mx-10 flex items-center flex-col gap-2 lg:order-first">
             <li className="mb-3 text-lg font-semibold">با ما همراه باشید</li>
@@ -18,20 +18,20 @@ const Footer = () => {
               <FaYoutube className="h-8 w-8" />
             </div>
           </ul>
-          <ul className="mx-16 flex my-8 flex-col gap-2">
+          <ul className="mx-16 sm:mx-8 flex my-8 flex-col gap-2">
             <li className="mb-3 text-lg font-semibold">تماس با ما</li>
             <li className="w-64">آدرس: تهران میدان آزادی خیابان استقلال</li>
             <li>شماره تماس: 23 999 623</li>
           </ul>
 
-          <ul className="mx-16 flex my-8 flex-col gap-2">
+          <ul className="mx-16 flex sm:mx-8 my-8 flex-col gap-2">
             <li className="mb-3 text-lg font-semibold">خدمات</li>
             <li><Link href="/immigration">مهاجرت</Link></li>
             <li><Link href="/#consult">مشاوره</Link></li>
             <li><Link href="/post/lottery">لاتاری</Link></li>
           </ul>
 
-          <ul className="mx-16 lg:mx-24 my-8 flex flex-col gap-2">
+          <ul className="mx-16 lg:mx-20 sm:mx-8 my-8 flex flex-col gap-2">
             <li className="mb-3 text-lg font-semibold">مقاله</li>
             <li><Link href="/post/education">تحصیلی</Link></li>
             <li><Link href="/post/work">کاری</Link></li>
@@ -45,7 +45,7 @@ const Footer = () => {
             alt="user icon"
             width={110}
             height={50}
-            className="order-first mx-auto lg:m-auto h-[100px] w-[100px] lg:order-last"
+            className="order-first m-auto  lg:m-auto h-[100px] w-[100px] lg:order-last"
           />
         </div>
       </div>
